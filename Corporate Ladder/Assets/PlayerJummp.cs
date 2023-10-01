df549e1c01ae7603d007095d8cd95c5912418ce6
eelkus01 <130937420+eelkus01@users.noreply.github.com>
2023-10-01 18:20:06 -0400

    Reverted back to old jump script, still not registering player is grounded but will fix later

--- Corporate Ladder/Assets/PlayerJummp.cs
+++ Corporate Ladder/Assets/PlayerJummp.cs
@@ -2,8 +2,7 @@ using System.Collections.Generic;
 using System.Collections;
 using UnityEngine;
 
-public class PlayerJump : MonoBehaviour
-{
+public class PlayerJump : MonoBehaviour {
 
     //public Animator anim;
     public Rigidbody2D rb;
@@ -11,32 +10,30 @@ public class PlayerJump : MonoBehaviour
     public Transform feet;
     public LayerMask groundLayer;
     public LayerMask enemyLayer;
-    public bool canJump = true;
+    public bool canJump = false;
+    public int jumpTimes = 0;
     public bool isAlive = true;
-    //public AudioSource JumpSFX; 
+    //public AudioSource JumpSFX;
 
-    void Start()
-    {
-        //anim = gameObject.GetComponentInChildren<Animator>(); 
+    void Start(){
+        //anim = gameObject.GetComponentInChildren<Animator>();
         rb = GetComponent<Rigidbody2D>();
     }
 
-    void Update()
-    {
-        if (IsGrounded()) {
-            canJump = true;
-        } else {
-            canJump = false;
+    void Update() {
+        if ((IsGrounded()) || (jumpTimes <= 1)){
+                canJump = true;
+        }  else if (jumpTimes > 1){
+                canJump = false;
         }
 
-        if ((Input.GetButtonDown("Jump")) && (canJump) && (isAlive == true))
-        {
-            Jump();
+        if ((Input.GetButtonDown("Jump")) && (canJump) && (isAlive == true)) {
+                Jump();
         }
     }
 
-    public void Jump()
-    {
+    public void Jump() {
+        jumpTimes += 1;
         rb.velocity = Vector2.up * jumpForce;
         // anim.SetTrigger("Jump");
         // JumpSFX.Play();
@@ -45,14 +42,14 @@ public class PlayerJump : MonoBehaviour
         //rb.velocity = movement;
     }
 
-    public bool IsGrounded()
-    {
-        Collider2D groundCheck = Physics2D.OverlapCircle(feet.position, 10f, groundLayer);
-        if ((groundCheck != null) )
-        {
-            //Debug.Log("I am trouching ground!");
-            return true;
+    public bool IsGrounded() {
+        Collider2D groundCheck = Physics2D.OverlapCircle(feet.position, 2f, groundLayer);
+        Collider2D enemyCheck = Physics2D.OverlapCircle(feet.position, 2f, enemyLayer);
+        if ((groundCheck != null) || (enemyCheck != null)) {
+                Debug.Log("I am touching ground!");
+                jumpTimes = 0;
+                return true;
         }
         return false;
-    }
+      }
 }
